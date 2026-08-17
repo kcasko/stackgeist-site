a7e138df5edcfa386eebf886adb9b81a205e9597
Keith Casko <kcasko@wgu.edu>
2026-08-16 21:28:23 -0400

    Add StackGeist trust compliance and SEO foundation

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,11 @@
+﻿export const prerender = true;
+const routes = [
+  '/', '/about', '/contact', '/privacy', '/affiliate-disclosure',
+  '/setups/gaming', '/setups/midnight-shift', '/gear/desk-lighting',
+  '/gear/desk-lighting/benq-screenbar-pro', '/gear/desk-lighting/luminoodle',
+];
+export function GET() {
+  const urls = routes.map((route) => `<url><loc>https://stackgeist.dev${route}</loc></url>`).join('');
+  const body = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}</urlset>`;
+  return new Response(body, { headers: { 'Content-Type': 'application/xml; charset=utf-8' } });
+}
