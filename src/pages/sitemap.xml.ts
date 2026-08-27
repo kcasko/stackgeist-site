bcc28eb0317d0f5e4802bc14e1b34abd2c045d87
Keith Casko <kcasko@wgu.edu>
2026-08-27 03:58:14 -0400

    Add verified Anker A9196 fit guide

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -4,7 +4,7 @@ const routes = [
   '/setups/gaming', '/setups/midnight-shift', '/setups/small-bedroom-gaming', '/setups/budget-gaming-desk',
   '/guides/desk-layout-basics', '/guides/cable-management', '/guides/cheap-desk-upgrades', '/gear', '/gear/desk-lighting',
   '/gear/desk-lighting/benq-screenbar-pro', '/gear/desk-lighting/luminoodle',
-  '/gear/desk-power/anker-525', '/gear/desk-surface/steelseries-qck-heavy-xxl',
+  '/gear/desk-power/anker-525', '/gear/desk-power/anker-nano-a9196', '/gear/desk-surface/steelseries-qck-heavy-xxl',
 ];
 export function GET() {
   const urls = routes.map((route) => `<url><loc>https://stackgeist.dev${route}</loc></url>`).join('');
